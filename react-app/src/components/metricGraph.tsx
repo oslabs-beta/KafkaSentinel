e5af7c679f5849a0e316015d5ea125e426da8138
nkumar125 <38634288+nkumar125@users.noreply.github.com>
2021-07-08 10:36:48 -0400

    socketio working on back end to emit and use messages

--- react-app/src/components/metricGraph.tsx
+++ react-app/src/components/metricGraph.tsx
@@ -1,13 +1,34 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { Line } from "react-chartjs-2";
 import { Bar } from "react-chartjs-2"
-import "chartjs-plugin-streaming";
+// added
+const { io } = require ('socket.io-client');
+// import "chartjs-plugin-streaming";
 
 
 const metricGraph = props => {
+
+  // const { io } = require ('socket.io-client');
+
+  const socket = io("http://localhost:5000");
+
+  let totalMessages = 0;
+
+  useEffect( () => {
+    // client-side
+    socket.on("total messages", data => {
+      totalMessages = data;
+    });
+  })
+  
+  // // client-side
+  // socket.on("message", data => {
+  //   totalProducerBytes = data;
+  // });
+
   return(
     //single metric graph component
-    <div></div>
+    <div>Total Producer Messages: {totalMessages}</div>
   )
 }
 
